c09b59ae19418a801c0a468753031a46d7fd65eb
Rafael Figueiredo <rafaelfigueiredo196@gmail.com>
2026-06-10 23:35:31 -0300

    feat: vpc do laboratorio com dns habilitado

---
+++ terraform/network.tf
@@ -0,0 +1,12 @@
+# ----------------------------------------
+# VPC - a rede privada do projeto
+# ----------------------------------------
+resource "aws_vpc" "lab" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "lab-vpc"
+  }
+}
